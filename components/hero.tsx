66c0b0e7bea57ecab99d23cd277101f8122b5a49
Kaushik Varanasi <kaushik@Kaushiks-MacBook-Pro.local>
2024-02-14 11:15:05 +0530

    hooray release emoji and better right arrow

--- components/hero.tsx
+++ components/hero.tsx
@@ -127,7 +127,11 @@ export default function Hero() {
               <div className="inline-flex relative before:absolute before:inset-0 before:bg-purple-500 before:blur-md">
                 <a className="btn-sm py-0.5 text-slate-300 hover:text-white transition duration-150 ease-in-out group [background:linear-gradient(theme(colors.purple.500),_theme(colors.purple.500))_padding-box,_linear-gradient(theme(colors.purple.500),_theme(colors.purple.200)_75%,_theme(colors.transparent)_100%)_border-box] relative before:absolute before:inset-0 before:bg-slate-800/50 before:rounded-full before:pointer-events-none shadow" href="https://github.com/RocketsGraphQL/rgraph/releases/tag/v0.2-stable">
                   <span className="relative inline-flex items-center">
-                    Checkout the v0.2.0 stable release <span className="tracking-normal text-purple-500 group-hover:translate-x-0.5 transition-transform duration-150 ease-in-out ml-1">-&gt;</span>
+                  🎉 &nbsp; Checkout the v0.2.0 stable release 
+                  {/* <span className="tracking-normal text-purple-500 group-hover:translate-x-0.5 transition-transform duration-150 ease-in-out ml-1">
+
+                  </span> */}
+                  <svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg" className="ml-1 h-4 w-4"><path d="M8.14645 3.14645C8.34171 2.95118 8.65829 2.95118 8.85355 3.14645L12.8536 7.14645C13.0488 7.34171 13.0488 7.65829 12.8536 7.85355L8.85355 11.8536C8.65829 12.0488 8.34171 12.0488 8.14645 11.8536C7.95118 11.6583 7.95118 11.3417 8.14645 11.1464L11.2929 8H2.5C2.22386 8 2 7.77614 2 7.5C2 7.22386 2.22386 7 2.5 7H11.2929L8.14645 3.85355C7.95118 3.65829 7.95118 3.34171 8.14645 3.14645Z" fill="currentColor" fill-rule="evenodd" clip-rule="evenodd"></path></svg>
                   </span>
                 </a>
               </div>
