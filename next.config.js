388a2467ae0eadf3e9ff707b1c24ac329ace882b
Kaushik Varanasi <kaushik.varanasi1@gmail.com>
2024-03-22 08:52:05 +0530

    Clickjacking fixes (#13)
    * Ai chatbots cta and spelling changes
    
    * remove click.html
    
    ---------
    
    Co-authored-by: Kaushik Varanasi <kaushik@Kaushiks-MacBook-Pro.local>

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
+
+// avoid clickjacking
+const securityHeaders = [
+  {
+    key: 'X-Frame-Options',
+    value: 'SAMEORIGIN'
+  },
+]
+
 const nextConfig = {
     eslint: {
         // Warning: This allows production builds to successfully complete even if
@@ -12,6 +21,15 @@ const nextConfig = {
           hostname: 'production-customer-site-public.s3.us-east-2.amazonaws.com',
         },
       ],
+    },
+    async headers() {
+      return [
+        {
+          // Apply these headers to all routes in your application.
+          source: '/(.*)',
+          headers: securityHeaders,
+        },
+      ]
     }
 }
 
